8054ec28a6ffc3ffaf85dba5d0a67bf189db92f6
FreitasGa <freitasga.01@gmail.com>
2021-07-13 12:08:18 -0300

    feat: Setting up amplify

---
+++ src/config.js
@@ -0,0 +1,12 @@
+export const config = {
+  apiGateway: {
+    REGION: 'sa-east-1',
+    URL: 'https://570xh70p77.execute-api.sa-east-1.amazonaws.com/dev',
+  },
+  cognito: {
+    REGION: 'sa-east-1',
+    USER_POOL_ID: 'sa-east-1_rdmgYifuJ',
+    APP_CLIENT_ID: '3k96ac9f0t6cdap8o1h37hnid0',
+    IDENTITY_POOL_ID: 'sa-east-1:de0ccade-58fe-490d-b01f-60b295d9e900',
+  },
+};
--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Amplify } from 'aws-amplify';
 import { App } from './App';
 
+import { config } from './config';
 import './global.scss';
+
+Amplify.configure({
+  Auth: {
+    mandatorySignIn: false,
+    region: config.cognito.REGION,
+    userPoolId: config.cognito.USER_POOL_ID,
+    identityPoolId: config.cognito.IDENTITY_POOL_ID,
+    userPoolWebClientId: config.cognito.APP_CLIENT_ID,
+  },
+  API: {
+    endpoints: [
+      {
+        name: 'post-by',
+        endpoint: config.apiGateway.URL,
+        region: config.apiGateway.REGION,
+      },
+    ],
+  },
+});
 
 ReactDOM.render(
   <React.StrictMode>

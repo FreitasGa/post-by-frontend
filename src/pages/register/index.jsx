8ede747ff37e11c15b91afb692ce2cc02b2e1e61
FreitasGa <freitasga.01@gmail.com>
2021-07-13 19:51:21 -0300

    feat: add register

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -1,62 +1,160 @@
 import { useState } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import { Auth } from 'aws-amplify';
+import toast from 'react-hot-toast';
 
+import { useAppContext } from '../../libs/contextLib';
 import { Header } from '../../components/header';
+import { LoaderButton } from '../../components/loaderButton';
 
 import './styles.scss';
 
 export function Register() {
+  const history = useHistory();
+  const { userHasAuthenticated } = useAppContext();
+
   const [name, setName] = useState('');
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+  const [confirmationCode, setConfirmationCode] = useState('');
+
+  const [newUser, setNewUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
 
   function validateForm() {
-    return email.length > 0 && password.length > 0;
+    return (
+      name.length > 0 &&
+      email.length > 0 &&
+      password.length > 0 &&
+      password === confirmPassword
+    );
+  }
+
+  function validateConfirmationForm() {
+    return confirmationCode.length > 0;
   }
 
   async function handleSubmit(event) {
     event.preventDefault();
+    setIsLoading(true);
+    try {
+      const newUser = await Auth.signUp({
+        username: email,
+        password: password,
+        attributes: {
+          name: name,
+        },
+      });
+      setIsLoading(false);
+      setNewUser(newUser);
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+      setName('');
+      setEmail('');
+      setPassword('');
+      setConfirmPassword('');
+    }
+  }
+
+  async function handleConfirmationSubmit(event) {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      await Auth.confirmSignUp(email, confirmationCode);
+      await Auth.signIn(email, password);
+      userHasAuthenticated(true);
+      setIsLoading(false);
+      history.push('/');
+      toast.success('Conta criada com sucesso!');
+    } catch (error) {
+      setIsLoading(false);
+      toast.error(error.message);
+      setEmail('');
+      setPassword('');
+    }
+  }
+
+  function RegisterForm() {
+    return (
+      <div className="RegisterBody">
+        <h1>Cadastro</h1>
+        <form className="RegisterForm" onSubmit={handleSubmit}>
+          <div className="RegisterFormInput">
+            <input
+              type="text"
+              placeholder="Nome"
+              onChange={(e) => setName(e.target.value)}
+              value={name}
+              autoFocus
+            />
+            <input
+              type="email"
+              placeholder="E-mail"
+              onChange={(e) => setEmail(e.target.value)}
+              value={email}
+            />
+            <input
+              type="password"
+              placeholder="Senha"
+              onChange={(e) => setPassword(e.target.value)}
+              value={password}
+            />
+            <input
+              type="password"
+              placeholder="Confirme a Senha"
+              onChange={(e) => setConfirmPassword(e.target.value)}
+              value={confirmPassword}
+            />
+          </div>
+          <LoaderButton
+            type="submit"
+            className="RegisterSubmit"
+            isLoading={isLoading}
+            disabled={!validateForm()}
+          >
+            Criar Conta
+          </LoaderButton>
+        </form>
+        <Link to="/login">Entre com sua conta</Link>
+      </div>
+    );
+  }
+
+  function ConfirmationForm() {
+    return (
+      <div className="RegisterBody">
+        <h1>Confirme o codigo</h1>
+        <form className="RegisterForm" onSubmit={handleConfirmationSubmit}>
+          <div className="RegisterFormInput">
+            <p>Verifique o código que foi enviado ao seu e-mail</p>
+            <input
+              type="text"
+              placeholder="Codigo de confirmação"
+              onChange={(e) => setConfirmationCode(e.target.value)}
+              value={confirmationCode}
+              autoFocus
+            />
+          </div>
+          <LoaderButton
+            type="submit"
+            className="RegisterSubmit"
+            isLoading={isLoading}
+            disabled={!validateConfirmationForm()}
+          >
+            Verificar
+          </LoaderButton>
+        </form>
+      </div>
+    );
   }
 
   return (
     <>
       <Header />
       <div className="RegisterWrapper">
-        <div className="RegisterBody">
-          <h1>Cadastro</h1>
-          <form className="RegisterForm" onSubmit={handleSubmit}>
-            <div className="RegisterFormInput">
-              <input
-                type="text"
-                placeholder="Nome"
-                onChange={(e) => setName(e.target.value)}
-                value={name}
-                autoFocus
-              />
-              <input
-                type="email"
-                placeholder="E-mail"
-                onChange={(e) => setEmail(e.target.value)}
-                value={email}
-              />
-              <input
-                type="password"
-                placeholder="Senha"
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-              />
-            </div>
-            <button
-              type="submit"
-              className="RegisterSubmit"
-              disabled={!validateForm()}
-            >
-              Entrar
-            </button>
-          </form>
-          <Link to="/login">Entre com sua conta</Link>
-        </div>
+        {newUser === null ? RegisterForm() : ConfirmationForm()}
       </div>
     </>
   );
